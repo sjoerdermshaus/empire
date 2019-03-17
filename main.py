481ce0eff4f974766d4cd50ffe9b71f1bb0d4f7f
sjoerdermshaus <sjoerdermshaus@gmail.com>
2019-03-17 09:30:00 +0100

    main added

---
+++ main.py
@@ -0,0 +1,27 @@
+from empire_scraper.src.empire_movies import EmpireMovies
+import yaml
+from logging.config import dictConfig
+from empire_scraper.src.empire_helpers import print_movies
+import logging
+
+
+def main():
+    import os
+    print(os.getcwd())
+    with open('root.yaml', 'r') as fh:
+        config_root = yaml.load(fh.read(), Loader=yaml.FullLoader)
+    dictConfig(config_root)
+    _ = logging.getLogger('root')
+    my_pages = [446, 447, 68]
+    my_article_numbers = [5, 1, 20]
+    my_pages = list(range(1, 551))
+    my_article_numbers = None
+    my_movies = EmpireMovies(process_images=False,
+                             number_of_processors=10,
+                             use_proxies=True,
+                             max_number_of_attempts=5).get_movies(my_pages, my_article_numbers)
+    # print_movies(my_movies)
+
+
+if __name__ == '__main__':
+    main()
